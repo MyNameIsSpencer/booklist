49018a0c8d368e3799203b707787f113d02f67e4
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-12-14 18:40:46 -0500

    done

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,16 @@
 class BooksController < ApplicationController
   def index
-
+    @books = Book.all
+    respond_to do |format|
+      format.text
+      format.html
+      format.csv do
+        render plain: Book.generate_csv(@books)
+      end
+      format.json do
+        render json: @books
+      end
+      format.xml { render xml: @books }
+    end
   end
-
 end
